d01c434983f4faf50487a7d65aff7108240160eb
Macmillan1411 <macmillantapiwanashe1411@gmail.com>
2025-04-11 00:23:29 +0300

    added validations for reservation input

--- app/schemas/reservation.py
+++ app/schemas/reservation.py
@@ -1,13 +1,23 @@
-from datetime import datetime
-
-from pydantic import BaseModel
+from datetime import datetime, timedelta
+from pydantic import BaseModel, Field
 
 
 class ReservationCreate(BaseModel):
-    customer_name: str
-    table_id: int
-    reservation_time: datetime
-    duration_minutes: int
+    customer_name: str = Field(
+        min_length=2,
+        max_length=50,
+    )
+    table_id: int = Field(
+        gt=0,
+    )
+    reservation_time: datetime = Field(
+        gt=datetime.now(),
+        lt=datetime.now() + timedelta(days=30),
+    )
+    duration_minutes: int = Field(
+        gt=10,
+        le=240,  # 4 hours maximum
+    )
 
 
 class ReservationRead(BaseModel):
